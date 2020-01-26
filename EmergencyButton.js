0c561312cddb99d6a3d874dab02fa324a1935760
Bryan Ofuokwu <bryanofuokwu@gmail.com>
2020-01-26 00:05:04 -0600

    emergency button

---
+++ EmergencyButton.js
@@ -0,0 +1,129 @@
+import React, { Component } from "react";
+import {
+  StyleSheet,
+  Text,
+  View,
+  ScrollView,
+  TextInput,
+  Button,
+  TouchableOpacity,
+  Image
+} from "react-native";
+import { Button2 } from "./Button";
+
+export default class Emergency extends React.Component {
+  render() {
+    return (
+      <View style={styles.main}>
+        <Text
+          style={{
+            fontWeight: "bold",
+            color: "white",
+            marginTop: 80,
+            fontSize: 40,
+            textAlign: "center"
+          }}
+        >
+          {" "}
+          Emergency Type
+        </Text>
+        <View style={styles.logo1}>
+          <Image
+            source={require("./assets/alert_icon.png")}
+            style={{ width: 75, height: 75 }}
+          />
+          <Button style={styles.Button} title="Emergency" />
+        </View>
+        <View style={styles.logo2}>
+          <Image
+            source={require("./assets/fire_icon.png")}
+            style={{ width: 75, height: 75 }}
+          />
+          <Button style={styles.Button} title="Fire" />
+        </View>
+        <View style={styles.logo3}>
+          <Image
+            source={require("./assets/animal_icon.png")}
+            style={{ width: 75, height: 75 }}
+          />
+          <Button style={styles.Button} title="Animal" />
+        </View>
+        {/* <View style={styles.container}>
+          <TextInput style={styles.input} placeholder="Username" type="email" />
+          <TextInput
+            style={styles.input}
+            placeholder="Enter Password"
+            secureTextEntry
+          />
+          <TouchableOpacity style={styles.btn}>
+            <Text>Login</Text>
+          </TouchableOpacity>
+        </View> */}
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  main: {
+    height: "100%",
+    width: "100%",
+    backgroundColor: "darkred",
+    paddingTop: 80
+  },
+  logo1: {
+    alignItems: "center",
+    justifyContent: "center",
+    height: 100,
+    top: 100
+  },
+  logo2: {
+    alignItems: "center",
+    justifyContent: "center",
+    right: 90,
+    top: 90,
+    height: 100
+  },
+  logo3: {
+    alignItems: "center",
+    justifyContent: "center",
+    left: 90,
+    top: 0
+  },
+  container: {
+    flex: 1,
+    backgroundColor: "darkred",
+    alignItems: "center",
+    marginTop: 10
+  },
+
+  input: {
+    height: 40,
+    width: "90%",
+    borderRadius: 20,
+    borderColor: "black",
+    backgroundColor: "indianred",
+    borderWidth: 3,
+    padding: 10,
+    margin: 10
+  },
+  btn: {
+    width: 200,
+    height: 50,
+    backgroundColor: "deepskyblue",
+    textAlign: "center",
+    borderRadius: 40,
+    alignItems: "center",
+    justifyContent: "center",
+    marginTop: 10
+  },
+
+  emergency_header: {
+    backgroundColor: "#00aeef",
+    width: "100%",
+    padding: 20,
+    alignItems: "center",
+    justifyContent: "center",
+    borderRadius: 8
+  }
+});
